82f10503a44b61561fd8058980bf43efb4c15e3b
rizkitriananda <riskitriananda321@gmail.com>
2026-08-04 04:47:49 +0700

    feat: add forgot password modal

--- app/(auth)/forgot-password/page.tsx
+++
@@ -1,9 +0,0 @@
-import ForgotPasswordForm from "@/components/features/auth/ForgotPasswordForm";
-
-export default function ForgotPasswordPage() {
-  return (
-    <div className="flex-1 flex items-center justify-center p-6 sm:p-12">
-      <ForgotPasswordForm />
-    </div>
-  );
-}
